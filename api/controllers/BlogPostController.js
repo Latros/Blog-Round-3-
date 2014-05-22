a5f5b8b5b0086880c0c826d4fa61ff7fd5a5a144
Samuel Stiles <sam@phasesolutions.ca>
2014-05-22 00:56:24 -0300

    Added stuffs

---
+++ api/controllers/BlogPostController.js
@@ -0,0 +1,11 @@
+/**
+ * BlogPostController
+ *
+ * @description :: Server-side logic for managing Blogposts
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
